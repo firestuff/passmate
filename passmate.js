28e5c2c0d6f67fbd0b5c9b20f4e781e77d128895
Ian Gulliver <ian@flamingcow.tv>
2018-12-08 01:17:40 +0000

    Fix syntax error

--- passmate.js
+++ passmate.js
@@ -109,7 +109,7 @@ class PassMate {
 		let print = this.addElement('button', printStep, 'Click here to print the book!');
 		print.style.cursor = 'pointer';
 		printStep.addEventListener('click', () => {
-			let ga = window.ga || () => {};
+			let ga = window.ga || (() => {});
 			ga('send', 'event', 'Overview', 'print');
 			window.print();
 		});
